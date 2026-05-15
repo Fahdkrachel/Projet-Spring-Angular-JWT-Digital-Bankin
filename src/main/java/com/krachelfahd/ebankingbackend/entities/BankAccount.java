1ded42188992a07f5f3d0d09bd98a36a5f199cd7
FahD <afkarfhd@gmail.com>
2026-05-15 23:56:13 +0000

    mapping des entities

--- src/main/java/com/krachelfahd/ebankingbackend/entities/BankAccount.java
+++ src/main/java/com/krachelfahd/ebankingbackend/entities/BankAccount.java
@@ -1,6 +1,10 @@
 package com.krachelfahd.ebankingbackend.entities;
 
 import com.krachelfahd.ebankingbackend.enums.AccountStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,14 +13,18 @@ import java.util.Date;
 import java.util.List;
 
 @Data @NoArgsConstructor @AllArgsConstructor
+@Entity
 
 public class BankAccount {
+    @Id
     private String id;
     private Date created;
     private Double  balance;
     private AccountStatus status;
     private String currency;
+    @ManyToOne /*plusieurs comptes pour un client*/
     private Customer customer;
+    @OneToMany(mappedBy = "bankAccount")
     List<Operation> operationList;
 
 
--- src/main/java/com/krachelfahd/ebankingbackend/entities/Operation.java
+++ src/main/java/com/krachelfahd/ebankingbackend/entities/Operation.java
@@ -1,20 +1,26 @@
 package com.krachelfahd.ebankingbackend.entities;
 
 import com.krachelfahd.ebankingbackend.enums.OpType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 
 import java.util.Date;
 
 @Data
 @NoArgsConstructor @AllArgsConstructor
+@Entity
 
 public class Operation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Date date;
     private double amount;
     private OpType type;
+    @ManyToOne
     private BankAccount bankAccount;
 
 }
